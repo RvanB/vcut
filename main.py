e22a3e01341b4acc339d4ef984b4d39894c89e9b
RvanB <rvanbronkhorst@gmail.com>
2025-05-14 21:52:53 -0700

    Separate functions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from numba import njit, prange
 import cv2
 
 
-def smooth_normals(img, d=20, sigmaColor=35):
+def smooth_normals(img, d=20, sigmaColor=25):
     img_np = np.array(img)
     smoothed = cv2.bilateralFilter(img_np, d, sigmaColor, 50)
     return Image.fromarray(smoothed)
@@ -18,32 +18,6 @@ def upscale(img, upscale_factor):
     img_np = np.array(img)
     img_np = zoom(img_np, upscale_factor, order=3)
     return Image.fromarray(img_np)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--image-file", help="Path to the input image", required=True
-    )
-    parser.add_argument(
-        "-o", "--output-file", help="Path to the output normal map", required=True
-    )
-    parser.add_argument(
-        "-u",
-        "--upscale-factor",
-        type=float,
-        help="How much to upscale the image prior to normal generation",
-    )
-    parser.add_argument(
-        "-t", "--threshold", help="Threshold for mask after upscaling", default=40
-    )
-    parser.add_argument(
-        "-s",
-        "--tangent-sample-size",
-        help="How far forward and backward to look on contour for tangent estimation",
-    )
-    args = parser.parse_args()
-    return args
 
 
 @njit
@@ -60,18 +34,40 @@ def normalize(v):
     return v / norm
 
 
+@njit
+def estimate_normal(
+    before_contour_point, closest_contour_point, after_contour_point, point
+):
+    # Vectors
+    contour_to_point = normalize(point - closest_contour_point)
+    before_vector = normalize(before_contour_point - closest_contour_point)
+    after_vector = normalize(after_contour_point - closest_contour_point)
+
+    angle_to_before = calculate_angle(contour_to_point, before_vector)
+    angle_to_after = calculate_angle(contour_to_point, after_vector)
+
+    if angle_to_before < angle_to_after:
+        tangent = normalize(closest_contour_point - before_contour_point)
+    else:
+        tangent = normalize(after_contour_point - closest_contour_point)
+
+    normal = -tangent[1], tangent[0]
+    return normal
+
+
 @njit(parallel=True)
 def compute_normals(
-    mask,
-    contourX,
-    contourY,
-    contour_id_map,
-    contour_index_map,
-    contours_flat,
-    contour_start_idx,
-    nx2d,
-    ny2d,
-    tangent_sample_size=4,
+        *,
+        mask,
+        contourX,
+        contourY,
+        contour_id_map,
+        contour_index_map,
+        contours_flat,
+        contour_start_idx,
+        normalX,
+        normalY,
+        tangent_sample_size
 ):
     h, w = mask.shape
     for y in prange(h):
@@ -99,34 +95,22 @@ def compute_normals(
             contour = contours_flat[start:end]
             n = end - start
 
-            contour_to_point = normalize(point - closest_contour_point)
-
             # Indices in contour before and after closest point
             i0 = max(c_idx - tangent_sample_size, 0)
             i1 = min(c_idx + tangent_sample_size, n - 1)
 
             # Get before and after point locations
-            before_point = np.array(list(contour[i0]))
-            after_point = np.array(list(contour[i1]))
+            before_contour_point = np.array(list(contour[i0]))
+            after_contour_point = np.array(list(contour[i1]))
 
-            # Neighbor vectors
-            before_vector = normalize(before_point - closest_contour_point)
-            after_vector = normalize(after_point - closest_contour_point)
-            # bvx, bvy = normalize(bvx - cx, bvy - cy)
-            # v2x, v2y = normalize(x2 - cx, y2 - cy)
+            normal = estimate_normal(
+                before_contour_point, closest_contour_point, after_contour_point, point
+            )
 
-            angle_to_before = calculate_angle(contour_to_point, before_vector)
-            angle_to_after = calculate_angle(contour_to_point, after_vector)
-
-            if angle_to_before < angle_to_after:
-                tangent = normalize(closest_contour_point - before_point)
-            else:
-                tangent = normalize(after_point - closest_contour_point)
-
-            nx2d[y, x], ny2d[y, x] = -tangent[1], tangent[0]
+            normalX[y, x], normalY[y, x] = normal
 
 
-def compute_contour_based_normals(mask, strength=1.0):
+def compute_contour_based_normals(mask, tangent_sample_size, strength):
     h, w = mask.shape
 
     contours = measure.find_contours(mask, 0.01)
@@ -168,16 +152,16 @@ def compute_contour_based_normals(mask, strength=1.0):
 
     # Populate normalX and normalY
     compute_normals(
-        mask,
-        contourX,
-        contourY,
-        contour_id_map,
-        contour_index_map,
-        contours_flat,
-        contour_start_idx,
-        normalX,
-        normalY,
-        8,
+        mask=mask,
+        contourX=contourX,
+        contourY=contourY,
+        contour_id_map=contour_id_map,
+        contour_index_map=contour_index_map,
+        contours_flat=contours_flat,
+        contour_start_idx=contour_start_idx,
+        normalX=normalX,
+        normalY=normalY,
+        tangent_sample_size=tangent_sample_size
     )
 
     normalZ = np.full_like(normalX, 1.0 / strength)
@@ -195,6 +179,41 @@ def compute_contour_based_normals(mask, strength=1.0):
     return Image.fromarray(normal_rgb, mode="RGB")
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--image-file", help="Path to the input image", required=True
+    )
+    parser.add_argument(
+        "-o", "--output-file", help="Path to the output normal map", required=True
+    )
+    parser.add_argument(
+        "-u",
+        "--upscale-factor",
+        type=float,
+        help="How much to upscale the image prior to normal generation"
+    )
+    parser.add_argument(
+        "-T", "--threshold", help="Threshold for mask after upscaling", default=40
+    )
+    parser.add_argument(
+        "-t",
+        "--tangent-sample-size",
+        type=int,
+        help="How far forward and backward to look on contour for tangent estimation",
+        default=4
+    )
+    parser.add_argument(
+        "-s",
+        "--strength",
+        type=float,
+        help="Strength of the generated normal",
+        default=1.0
+    )
+    args = parser.parse_args()
+    return args
+
+
 def main():
     args = parse_args()
 
@@ -210,7 +229,10 @@ def main():
     mask = np.array(img) > args.threshold
 
     print("Computing normals")
-    normal_map = compute_contour_based_normals(np.array(mask), strength=1.0)
+    normal_map = compute_contour_based_normals(
+        mask=np.array(mask),
+        tangent_sample_size=args.tangent_sample_size,
+        strength=args.strength)
 
     print("Smoothing normals")
     normal_map = smooth_normals(normal_map)
